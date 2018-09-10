c64503c3a6976e2b70f8f246674a9373da282a85
zijiren <327588233@qq.com>
2018-09-10 19:22:50 +0800

    自适应

--- 2018.9.6-Jp_web/js/script.js
+++ 2018.9.6-Jp_web/js/script.js
@@ -10,24 +10,28 @@ window.onload=function(){
 function dsq1(){
 	var i=1
 	var tupian6_background=document.getElementById("tupian6_background").className
+	var tupian6=document.getElementById("tupian6")
+	tupian6_width=window.getComputedStyle(tupian6,null).width;
+	tupian6_height=window.getComputedStyle(tupian6,null).height;
+	for(var i=1;i<7;i++){
+		var tupian6_backgroundi=tupian6_background+i
+		var tupian6_backgroundii=document.getElementById(tupian6_backgroundi)
+	
+		tupian6_backgroundii.style.cssText="height:"+tupian6_height+";"+"width:"+tupian6_width+";"
+		
+	}
+
 			t2=window.setInterval(refreshCount1, 1500);
 			function refreshCount1(){
 					if(i>=7){
 						i=1
 					}
-
 						n=i-1
-						b=i+1
-				document.getElementById('tupian6_background5').style.opacity='0.4';
-				document.getElementById('tupian6_background6').style.opacity='0.4';
-						if(n>0&&n<=6&&b>0&&b<7){
-							var tupian6_backgroundn=tupian6_background+n
-							var tupian6_backgroundb=tupian6_background+b
-				document.getElementById('tupian6_background5').style.opacity='0.4';
-				document.getElementById('tupian6_background6').style.opacity='0.4';	
-				document.getElementById(tupian6_backgroundn).style.opacity='0.4';
-				document.getElementById(tupian6_backgroundb).style.opacity='0.4';
-
+						document.getElementById('tupian6_background5').style.opacity='0.4';
+						document.getElementById('tupian6_background6').style.opacity='0.4';
+						if(n>0&&n<=6){
+						var tupian6_backgroundn=tupian6_background+n
+						document.getElementById(tupian6_backgroundn).style.opacity='0.4';
 						}
 				var tupian6_backgroundi=tupian6_background+i
 			
